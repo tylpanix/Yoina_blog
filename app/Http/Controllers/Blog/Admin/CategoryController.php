e8d7dd65d3f81f88d20d6a0d0d8dba39d306e3db
tylpanix <daryna.yoina@oa.edu.ua>
2025-06-17 22:59:00 +0000

    Лабораторна 5. Валідація даних та додавання категорій

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Blog\Admin;
 //use App\Http\Controllers\Blog\Admin\BaseController;
 use App\Models\BlogCategory;
 use Illuminate\Support\Str;
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
+use App\Http\Requests\BlogCategoryUpdateRequest;
+use App\Http\Requests\BlogCategoryCreateRequest;
+
 
 class CategoryController extends BaseController
 {
@@ -21,11 +24,31 @@ class CategoryController extends BaseController
     public function create()
     {
         //dd(__METHOD__);
+        $item = new BlogCategory();
+        $categoryList = BlogCategory::all();
+
+        return view('blog.admin.categories.edit', compact('item', 'categoryList'));
     }
 
-    public function store(Request $request)
+    public function store(BlogCategoryCreateRequest $request)
     {
         //dd(__METHOD__);
+        $data = $request->input(); //отримаємо масив даних, які надійшли з форми
+        if (empty($data['slug'])) { //якщо псевдонім порожній
+            $data['slug'] = Str::slug($data['title']); //генеруємо псевдонім
+        }
+
+        $item = (new BlogCategory())->create($data); //створюємо об'єкт і додаємо в БД
+
+        if ($item) {
+            return redirect()
+                ->route('blog.admin.categories.edit', [$item->id])
+                ->with(['success' => 'Успішно збережено']);
+        } else {
+            return back()
+                ->withErrors(['msg' => 'Помилка збереження'])
+                ->withInput();
+        }
     }
 
     public function show(string $id)
@@ -43,7 +66,7 @@ class CategoryController extends BaseController
 
     }
 
-    public function update(Request $request, string $id)
+    public function update(BlogCategoryUpdateRequest $request, $id)
     {
         //dd(__METHOD__);
         $item = BlogCategory::find($id);
