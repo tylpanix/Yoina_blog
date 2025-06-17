eb9f992cc151977f7139033cc904da7b61461ce9
tylpanix <daryna.yoina@oa.edu.ua>
2025-06-17 21:40:29 +0000

    Лабораторна 3. Контроллери, роути, шаблони

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestTestController;
+use App\Http\Controllers\Blog\PostController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,4 +16,9 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+});
+Route::resource('rest', RestTestController::class)->names('restTest');
+
+Route::group(['prefix' => 'blog'], function () {
+    Route::resource('posts', PostController::class)->names('blog.posts');
 });
