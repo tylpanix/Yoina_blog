b4e3e40dafbab79660f51d14947b99c9fbc51a75
tylpanix <daryna.yoina@oa.edu.ua>
2025-06-17 08:59:36 +0000

    Лабораторна 2. Створення міграцій і заповнення даних в таблицях

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call(UsersTableSeeder::class);
+        $this->call(BlogCategoriesTableSeeder::class);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        \App\Models\BlogPost::factory(100)->create();
     }
+
 }
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            [
+                'name' => 'Невідомий автор',
+                'email' => 'author_unknown@g.g',
+                'password' => bcrypt(Str::random(16)),
+            ],
+            [
+                'name' => 'Автор',
+                'email' => 'author1@g.g',
+                'password' => bcrypt('123123'),
+            ],
+        ];
+
+        DB::table('users')->insert($data);
+    }
+
+}
