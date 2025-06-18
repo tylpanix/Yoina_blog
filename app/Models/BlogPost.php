3ef3a8d10e68a715758cd5341792100717550968
tylpanix <daryna.yoina@oa.edu.ua>
2025-06-18 00:12:11 +0000

    Лабораторна 8. Керування статтями блога

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -11,5 +11,39 @@ class BlogPost extends Model
     use HasFactory;
     use SoftDeletes;
 
-    //
+    /**
+     * Масив доступних для масового заповнення полів
+     */
+    protected $fillable = [
+        'title',
+        'slug',
+        'category_id',
+        'excerpt',
+        'content_raw',
+        'is_published',
+        'published_at',
+        'user_id',
+    ];
+
+    /**
+     * Категорія статті
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        // Стаття належить категорії
+        return $this->belongsTo(BlogCategory::class);
+    }
+
+    /**
+     * Автор статті
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        // Стаття належить користувачу
+        return $this->belongsTo(User::class);
+    }
 }
